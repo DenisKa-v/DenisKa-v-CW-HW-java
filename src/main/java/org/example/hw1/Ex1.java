7dd06ce9ac362cfcb9d6815e39f1b7c173c08ded
DenisKa <pochta_dk@mail.ru>
2023-03-23 19:20:26 +0300

    Add Ex1

--- src/main/java/org/example/hw1/Ex1.java
+++ src/main/java/org/example/hw1/Ex1.java
@@ -1,4 +1,28 @@
 package org.example.hw1;
 
+import java.util.Scanner;
+
+// 151. Reverse Words in a String
 public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String str = scanner.nextLine();
+        String result = reverseWords(str);
+        System.out.printf("Развернутое выражение: %s", result);
+    }
+
+    /**
+     * @apiNote Разворот строки
+     * @param s строка
+     * @return строка
+     */
+    public static String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        String res = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            res += arr[i] + " ";
+        }
+        return res;
+    }
 }
