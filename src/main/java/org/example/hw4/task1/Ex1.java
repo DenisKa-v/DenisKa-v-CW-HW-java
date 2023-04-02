1501e521ed721170e55da5b63b8f7312564c57cd
DenisKa <pochta_dk@mail.ru>
2023-04-02 18:19:11 +0300

    Add Ex1 hw4

---
+++ src/main/java/org/example/hw4/task1/Ex1.java
@@ -0,0 +1,33 @@
+package org.example.hw4.task1;
+
+import java.util.LinkedList;
+
+/**
+ * Вывести список на экран в перевернутом виде (без массивов и ArrayList)
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        LinkedList inputLinkedList = new LinkedList<>();
+        for (int i = 0; i < 6; i++) {
+            inputLinkedList.add(i);
+        }
+        System.out.println(inputLinkedList);
+        System.out.println(inputLinkedList.size());
+        LinkedList reversLinkedList = reverseList(inputLinkedList);
+        System.out.println(reversLinkedList);
+    }
+
+    /**
+     * Разворот списка
+     *
+     * @param inputLinkedList входящий список
+     * @return развернутый список
+     */
+    private static LinkedList reverseList(LinkedList inputLinkedList) {
+        LinkedList resultLinkedList = new LinkedList<>();
+        while (inputLinkedList.size() > 0) {
+            resultLinkedList.addFirst(inputLinkedList.pop());
+        }
+        return resultLinkedList;
+    }
+}
